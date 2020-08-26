ef9ddc8e868d071def26bde40679f8e508e819ef
t-cochran <cochran@colorado.edu>
2020-08-25 23:58:54 -0600

    covered arrays

--- Week 1/week1_readings.scala
+++ Week 1/week1_readings.scala
@@ -3,7 +3,9 @@
   * 
   * Reference: https://www.scala-lang.org/api/current/scala/collection/immutable/List.html
   */
-object week1 {
+
+/* Lists : Immutable */
+object ListMethods {
 
     /**
       *  Main -- Program Entry
@@ -13,6 +15,9 @@ object week1 {
       */
     def main( args: Array[ String ] ) : Unit = {
         
+        /* Header */
+        println("[ List Methods ]" + "="*50 + "]\n");
+
         /* Create a list of integers */
         val x : List[ Int ] = List( 10, 111, 32, 14, 6, 8, 8, 8, 8, 32, 10, 86 );
         printList( x );
@@ -50,16 +55,84 @@ object week1 {
           */
         val x_sorted : List[ Int ] = x.sortWith( _ < _ );
         printList( x_sorted );
-        
+
+        /**
+          * Check out the printList_ using the underscore wildcard.
+          */
+        printList_( x );
+
+        /* Footer */
+        println("\n" + "-"*65 + "\n");
     }
 
-    /* ------------------------------------------------------------------------- */
+    /* --------------------------------------------------------------- */
 
     /* Method: Print the contents of a list */
     def printList( x : List[ Int ] ) : Unit = {
         for ( elt <- x ) {
             print( s"${ elt } ");
         }
-        println("");
+        println();
+    }
+
+    /* Method: Print contents of a list */
+    def printList_( x : List[ Int ] ) : Unit = {
+        x.foreach( print( _ ) );
+        print( "\n" );
+        x.foreach( y => print( s"${ y } " ) );
+        print( "\n" );
+    }
+}
+/* --------------------------------------------------------------- */
+
+/* Arrays : Mutable */
+object ArrayMethods {
+
+    /**
+      *  Main -- Program Entry
+      *
+      *  Note: 'Unit' is return type if nothing is returned
+      *        i.e. a void return.
+      */
+    def main( args: Array[ String ] ) : Unit = {
+        
+        /* Header */
+        println("[ Array Methods ]" + "="*50 + "]\n");
+
+        /* Create an array of integers */
+        val arr : Array[ Int ] = Array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 );
+        printArray( arr );
+
+        /* Change some values and print the result */
+        arr( 0 ) = 100;
+        arr( arr.size - 1 ) = 100;
+        printArray( arr );
+
+        /* Prepend and append values to the array */
+        val prepend = 1000 +: arr;
+        val append = arr :+ 1000;
+        printArray( prepend );
+        printArray( append );
+
+        /* Use some library functions */
+        println( s"Array sum: ${ arr.sum }" );
+        println( s"Array max: ${ arr.max }" );
+
+        /* Turn into a list! */
+        val arr_to_list = arr.toList;
+        for ( i <- 0.until( arr_to_list.size )) { 
+            print( s"${ arr_to_list( i ) } " ); 
+        }
+
+        /* Footer */
+        println("\n" + "-"*65 + "\n");
+    }
+
+    /* Method: Print contents of array using 'for-to-until' */
+    def printArray( x : Array[ Int ] ) : Unit = {
+        for ( i <- 0.until( x.length ) ) {
+            print( s"${ x(i) } " );
+        }
+        println();
     }
 }
