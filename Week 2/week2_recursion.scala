b19fa491d6a9b5bea6580751017441ae1c04d697
t-cochran <cochran@colorado.edu>
2020-08-29 14:46:17 -0600

    added recursive factorial function from readings

--- Week 2/week2_recursion.scala
+++ Week 2/week2_recursion.scala
@@ -8,8 +8,70 @@ object recursivePractice {
 
     def main( args : Array[ String ] ) : Unit = {
 
-        ??? // TODO
-        
+      func_2( 10 );
+
+      val test_factorial = factorial( 4 );
+      println( test_factorial );
+
     }
 
+
+
+
+
+    /**
+     * A function that returns x! using recursion 
+     */
+    def factorial( x : Int ) : Int = {
+
+      if ( x == 0 ) {
+
+        return 1;
+
+      }
+      else {
+
+        return x * factorial( x - 1 );
+
+      }
+
+    }
+
+    /**
+     * A scala function using a while loop. 
+     */
+    def func_1( x : Int ) : Int = {
+
+      var y = x;
+
+      while( y >= 0 ) {
+
+        println( y );
+
+        y = y - 1;
+
+      }
+
+      return 0;
+
+    }
+
+    /**
+     * func_1 using recursion 
+     */
+    def func_2( x : Int ) : Int = {
+
+      if ( x < 0 ) {
+
+        return x;
+
+      }
+
+      println( x );
+      
+      return func_2( x - 1 );
+
+    }
+
+      
 }
