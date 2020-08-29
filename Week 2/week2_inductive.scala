a7eb7ef04ef1f6791ca0bf41f51eb1fb64eccc78
t-cochran <cochran@colorado.edu>
2020-08-29 02:32:41 -0600

    minor format changes and added week 2 scratch code files

---
+++ Week 2/week2_inductive.scala
@@ -0,0 +1,15 @@
+/** 
+  * File: week2_inductive.scala 
+  * 
+  * Working through the material on inductively defined structures.
+  * 
+  */
+object inductiveStructs {
+    
+    def main( args : Array[ String ] ) : Unit = {
+
+        ??? // TODO
+        
+    }
+
+}
---
+++ Week 2/week2_recursion.scala
@@ -0,0 +1,15 @@
+/** 
+  * File: week2_recursion.scala 
+  * 
+  * Working through the material on recursion and induction.
+  * 
+  */
+object recursivePractice {
+
+    def main( args : Array[ String ] ) : Unit = {
+
+        ??? // TODO
+        
+    }
+
+}
