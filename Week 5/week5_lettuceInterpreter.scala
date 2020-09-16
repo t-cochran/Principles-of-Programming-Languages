6c6dd2078a23f943cbd56d088798e4d6d3f4cadb
t-cochran <cochran@colorado.edu>
2020-09-15 20:44:45 -0600

    added binary operations to the eval function

--- Week 5/week5_lettuceInterpreter.scala
+++ Week 5/week5_lettuceInterpreter.scala
@@ -11,25 +11,28 @@ sealed trait Program
 sealed trait Expr
 
 case class TopLevel( e : Expr ) extends Program
+
 case class Const( v : Double ) extends Expr
 case object True extends Expr
 case object False extends Expr
+
 case class Ident( s : String ) extends Expr
 
 case class Plus( e1: Expr, e2: Expr ) extends Expr
 case class Minus( e1: Expr, e2: Expr ) extends Expr
 case class Mult( e1: Expr, e2: Expr ) extends Expr
 case class Div( e1: Expr, e2: Expr ) extends Expr
+case class Geq( e1: Expr, e2: Expr ) extends Expr
+case class Eq( e1: Expr, e2: Expr ) extends Expr
+case class And( e1: Expr, e2: Expr ) extends Expr
+case class Or( e1: Expr, e2: Expr ) extends Expr
+
 case class Log( e: Expr ) extends Expr 
 case class Exp( e: Expr ) extends Expr
 case class Sine( e: Expr ) extends Expr
 case class Cosine( e: Expr ) extends Expr
-
-case class Geq( e1: Expr, e2:Expr ) extends Expr
-case class Eq( e1: Expr, e2: Expr ) extends Expr
-case class And( e1: Expr, e2: Expr ) extends Expr
-case class Or( e1: Expr, e2: Expr ) extends Expr
 case class Not( e: Expr ) extends Expr
+
 case class IfThenElse( e: Expr, eIf: Expr, eElse: Expr ) extends Expr
 
 case class Let( s: String, defExpr: Expr, bodyExpr: Expr ) extends Expr
@@ -52,13 +55,7 @@ sealed trait Value
 case class NumValue( d : Double ) extends Value
 case class BoolValue( b : Boolean ) extends Value
 case object ErrorValue extends Value
-
-
-
-
-
-
-
+/* --------------------------------------------------------------- */
 
 
 /**
@@ -66,10 +63,133 @@ case object ErrorValue extends Value
  */
 object lettuceEval {
 
+  /**
+   * Helper functions:
+   * 
+   * Convert a Lettuce value from the eval function to a double
+   * or boolean
+   */
+  def valConvert( v : Value ) : Double = {
+
+    ( v ) match {
+
+      case NumValue( d ) => d;
+      case _ => throw new IllegalArgumentException( s"Error converting value: $v" );
+
+    }
+
+  }
+
+  def boolConvert( v : Value ) : Boolean = {
+
+    ( v ) match {
+
+      case BoolValue( b ) => b;
+      case _ => throw new IllegalArgumentException( s"Error converting value: $v" );
+
+    }
+
+  }
+  /* --------------------------------------------------------------- */
+
+  /**
+   * Evaluate a lettuce expression for a given environment
+   */
+  def eval( e : Expr, env : Map[ String, Value ] ) : Value = {
+
+    /**
+     * Method for binary operations
+     * 
+     * Curried -- pass in expressions 1 and 2 and recursively evaluate them
+     * to their NumValue, then convert the NumValue to Double's.
+     * 
+     * Pass expressions 1 and 2 to whatever function 'f' you want.
+     */
+    def binaryNumOp( e1 : Expr, e2 : Expr )( f : ( Double, Double ) => Double ) = {
+      val val_1 : Double = valConvert( eval( e1, env ) );  // Eval expr 1
+      val val_2 : Double = valConvert( eval( e2, env ) );  // Eval expr 2
+      val funcResult : Double = f( val_1, val_2 );         // Pass expressions to 'f'
+      NumValue( funcResult );
+    }
+    def binaryBoolOp( e1 : Expr, e2 : Expr )( f : ( Double, Double ) => Boolean ) = {
+      val val_1 : Double = valConvert( eval( e1, env ) );  // Eval expr 1
+      val val_2 : Double = valConvert( eval( e2, env ) );  // Eval expr 2
+      val funcResult : Boolean = f( val_1, val_2 );        // Pass expressions to 'f'
+      BoolValue( funcResult );                        
+    }
+
+    /**
+     * Pattern match the expression passed
+     */
+    ( e ) match {
+
+      /**
+       * Base cases:
+       * 
+       * Constants do not require mappings in env; they are simply returned.
+       * Identifiers return the mapped value from the environment.
+       */
+      case Const( d ) => NumValue( d );
+      case Ident( d ) => {
+        if ( env contains d ) {
+          env( d );
+        }
+        else {
+          throw new IllegalArgumentException( s"Error: Ident( $d ) not found in env" );
+        }
+      }
+
+      /**
+       * Evaluate binary operations
+       * 
+       * Use the method 'binaryOp'. This will evaluate the NumValue of 
+       * e1 and e2 via recursion, then call the anonymous function on 
+       * the values of each expression.
+       */
+      case Plus( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ + _ )
+      case Minus( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ - _ )
+      case Mult( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ * _ )
+      case Geq( e1, e2 ) => binaryBoolOp( e1, e2 ) ( _ >= _ )
+      case Eq( e1, e2 ) => binaryBoolOp( e1, e2 ) ( _ == _ )
+      case Div( e1, e2 ) => binaryNumOp( e1, e2 ) {
+        case ( _, 0.0 ) => throw new IllegalArgumentException( s"Error: div by zero" );
+        case ( num, denom ) => num / denom;
+      }
+      case And( e1, e2 ) => {
+        val val_1 : Value = eval( e1, env ); // evaluate the first expression
+        ( val_1 ) match {
+          case BoolValue( false ) => BoolValue( false );  // short circuit
+          case BoolValue( true ) => {
+            val val_2 : Value = eval( e2, env );
+            ( val_2 ) match {
+              case BoolValue( _ ) => val_2;
+              case _ => throw new IllegalArgumentException( s"Error: And() expr not boolean" );
+            }
+          }
+        }
+      }
+      case Or( e1, e2 ) => {
+        val val_1 : Value = eval( e1, env );
+        ( val_1 ) match {
+          case BoolValue( true ) => BoolValue( true );  // short circuit
+          case BoolValue( false ) => {
+            val val_2 : Value = eval( e2, env );
+            ( val_2 ) match {
+              case BoolValue( _ ) => val_2; 
+              case _ => throw new IllegalArgumentException( s"Error: Or() expr not boolean" );
+            }
+          }
+        }
+      }
+
+
+
+    }
 
 
 
 
+  }
 
 
   def main( args : Array[ String ] ) : Unit = {
