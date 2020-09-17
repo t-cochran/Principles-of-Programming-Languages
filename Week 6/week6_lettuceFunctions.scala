fa666765038bd1adefe28f7c4693694d3630ee86
t-cochran <cochran@colorado.edu>
2020-09-16 23:32:05 -0600

    added week 6 files

---
+++ Week 6/week6_lettuceFunctions.scala
@@ -0,0 +1,396 @@
+/** 
+ * File: week6_lettuceFunctions.scala
+ * 
+ * Working through the material on the Let language
+ */
+
+/**
+ * Grammar for Lettuce types and expressions
+ * 
+ * Program  ->  TopLevel( Expr )
+ * 
+ * Expr   ->   Const( Number )
+ *        |    True
+ *        |    False
+ *        |    Ident( String )  
+ *        |    Plus( Expr, Expr )
+ *        |    Minus( Expr, Expr )
+ *        |    Mult( Expr, Expr )
+ *        |    Div( Expr, Expr )
+ *        |    Geq( Expr, Expr )
+ *        |    Eq( Expr, Expr )
+ *        |    And( Expr, Expr )
+ *        |    Or( Expr, Expr )
+ *        |    Log( Expr )
+ *        |    Exp( Expr )
+ *        |    Sine( Expr )
+ *        |    Cosine( Expr )
+ *        |    Not( Expr )
+ *        |    IfThenElse( Expr, Expr, Expr )
+ *        |    Let( String, Expr, Expr )
+ *        |    FunDef( String, Expr )
+ *        |    FunCall( Expr, Expr )
+ * 
+ * Double -> -2 | -1 | 0 | 1 | 2 ...
+ * String -> [a-zA-Z0-9_-]+
+ * Boolean -> true | false
+ */
+sealed trait Program
+sealed trait Expr
+
+case class TopLevel( e : Expr ) extends Program
+
+case class Const( v : Double ) extends Expr
+case object True extends Expr
+case object False extends Expr
+
+case class Ident( s : String ) extends Expr
+
+case class Plus( e1: Expr, e2: Expr ) extends Expr
+case class Minus( e1: Expr, e2: Expr ) extends Expr
+case class Mult( e1: Expr, e2: Expr ) extends Expr
+case class Div( e1: Expr, e2: Expr ) extends Expr
+case class Geq( e1: Expr, e2: Expr ) extends Expr
+case class Eq( e1: Expr, e2: Expr ) extends Expr
+case class And( e1: Expr, e2: Expr ) extends Expr
+case class Or( e1: Expr, e2: Expr ) extends Expr
+
+case class Log( e: Expr ) extends Expr 
+case class Exp( e: Expr ) extends Expr
+case class Sine( e: Expr ) extends Expr
+case class Cosine( e: Expr ) extends Expr
+case class Not( e: Expr ) extends Expr
+
+case class IfThenElse( e: Expr, eIf: Expr, eElse: Expr ) extends Expr
+
+case class Let( s: String, defExpr: Expr, bodyExpr: Expr ) extends Expr
+case class FunDef( param: String, bodyExpr: Expr ) extends Expr
+case class FunCall( funCalled: Expr, argExpr: Expr ) extends Expr
+/* --------------------------------------------------------------- */
+
+/**
+ * Grammar for Lettuce values that are evaluated
+ *
+ * Value -> NumValue( Double )
+ *       |  BoolValue( Boolean )
+ *       |  ErrorValue
+ * 
+ * Double -> -2 | -1 | 0 | 1 | 2 . . .
+ * Boolean -> true | false
+ * 
+ */
+sealed trait Value
+case class NumValue( d : Double ) extends Value
+case class BoolValue( b : Boolean ) extends Value
+case object ErrorValue extends Value
+/* --------------------------------------------------------------- */
+
+
+/**
+ * Lettuce eval function that takes a Lettuce program written in
+ * abstract syntax, and evaluates the program to return a result
+ */
+object lettuceEval {
+
+  /**
+   * Helper functions:
+   * 
+   * Convert a Lettuce value from the eval function to a double
+   * or boolean
+   */
+  def valConvert( v : Value ) : Double = {
+
+    ( v ) match {
+
+      case NumValue( d ) => d;
+      case _ => throw new IllegalArgumentException( s"Error converting value: $v" );
+
+    }
+
+  }
+
+  def boolConvert( v : Value ) : Boolean = {
+
+    ( v ) match {
+
+      case BoolValue( b ) => b;
+      case _ => throw new IllegalArgumentException( s"Error converting value: $v" );
+
+    }
+
+  }
+  /* --------------------------------------------------------------- */
+
+  /**
+   * Evaluate a lettuce expression for a given environment
+   */
+  def eval( e : Expr, env : Map[ String, Value ] ) : Value = {
+
+    /**
+     * Methods that get values from either a binary, unary or boolean operation,
+     * then perform a given function 'f' on those values, and return the result.
+     * 
+     * Curried -- pass in expressions 1 and 2 and recursively evaluate them
+     * to their NumValue, then convert the NumValue to Double's.
+     * 
+     * Pass expressions 1 and 2 to whatever function 'f' you want.
+     */
+    def binaryNumOp( e1 : Expr, e2 : Expr )( f : ( Double, Double ) => Double ) = {
+      val val_1 : Double = valConvert( eval( e1, env ) );  // Eval expr 1
+      val val_2 : Double = valConvert( eval( e2, env ) );  // Eval expr 2
+      val funcResult : Double = f( val_1, val_2 );         // Pass expressions to 'f'
+      NumValue( funcResult );
+    }
+    def boolOp( e1 : Expr, e2 : Expr )( f : ( Double, Double ) => Boolean ) = {
+      val val_1 : Double = valConvert( eval( e1, env ) );  // Eval expr 1
+      val val_2 : Double = valConvert( eval( e2, env ) );  // Eval expr 2
+      val funcResult : Boolean = f( val_1, val_2 );        // Pass expressions to 'f'
+      BoolValue( funcResult );                        
+    }
+    def unaryOp( e1 : Expr )( f : ( Double ) => Double ) ={
+      val val_1 : Double = valConvert( eval( e1, env ) );
+      val funcResult : Double = f( val_1 );
+      NumValue( funcResult );
+    }
+
+    /**
+     * Pattern match the expression passed
+     */
+    ( e ) match {
+
+      /**
+       * Base cases:
+       * 
+       * Constants and booleans do not require mappings in env; they are returned.
+       * Identifiers return the mapped value from the environment.
+       */
+      case Const( d ) => NumValue( d );
+      case True => BoolValue( true );
+      case False => BoolValue( false );
+      case Ident( d ) => {
+        if ( env contains d ) {
+          env( d );
+        }
+        else {
+          throw new IllegalArgumentException( s"Error: Ident( $d ) not found in env" );
+        }
+      }
+
+      /**
+       * Evaluate binary expressions
+       * 
+       * Use the method 'binaryOp' or 'boolOp' which evaluate e1 and e2 via recursion, 
+       * then call the anonymous function on the values of each expression.
+       */
+      case Plus( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ + _ )
+      case Minus( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ - _ )
+      case Mult( e1, e2 ) => binaryNumOp( e1, e2 ) ( _ * _ )
+      case Geq( e1, e2 ) => boolOp( e1, e2 ) ( _ >= _ )
+      case Eq( e1, e2 ) => boolOp( e1, e2 ) ( _ == _ )
+      case Div( e1, e2 ) => binaryNumOp( e1, e2 ) {
+        case ( _, 0.0 ) => throw new IllegalArgumentException( s"Error: div by zero" );
+        case ( num, denom ) => num / denom;
+      }
+      case And( e1, e2 ) => {
+        val val_1 : Value = eval( e1, env ); // evaluate the first expression
+        ( val_1 ) match {
+          case BoolValue( false ) => BoolValue( false );  // short circuit
+          case BoolValue( true ) => {
+            val val_2 : Value = eval( e2, env );
+            ( val_2 ) match {
+              case BoolValue( _ ) => val_2;
+              case _ => throw new IllegalArgumentException( s"Error: And() expr not boolean" );
+            }
+          }
+          case _ => throw new IllegalArgumentException( s"Error: And() expr not boolean" );
+        }
+      }
+      case Or( e1, e2 ) => {
+        val val_1 : Value = eval( e1, env );
+        ( val_1 ) match {
+          case BoolValue( true ) => BoolValue( true );  // short circuit
+          case BoolValue( false ) => {
+            val val_2 : Value = eval( e2, env );
+            ( val_2 ) match {
+              case BoolValue( _ ) => val_2; 
+              case _ => throw new IllegalArgumentException( s"Error: Or() expr not boolean" );
+            }
+          }
+          case _ => throw new IllegalArgumentException( s"Error: Or() expr not boolean" );
+        }
+      }
+
+      /**
+       * Evaluate unary expressions
+       * 
+       * Use the method 'unaryOp'. This will evaluate the NumValue of 
+       * e1 and e2 via recursion, then call the anonymous function on 
+       * the values of each expression.
+       */
+      case Log( e ) => unaryOp( e ) {
+        case v if v > 0.0 => math.log( v )
+        case v => throw new IllegalArgumentException( 
+          s"Log of a negative number ${ e } evaluates to ${ v }!"
+        )
+      }
+      case Exp( e ) => unaryOp( e ) ( math.exp )
+      case Sine( e ) => unaryOp( e ) ( math.sin )
+      case Cosine( e ) => unaryOp( e ) ( math.cos )
+      case Not( e ) => {
+        val val_1 : Value = eval( e, env )
+        ( val_1 ) match {
+          case BoolValue( b ) => BoolValue( !b )
+          case _ => throw new IllegalArgumentException( 
+            s"Not of a non-boolean expr: ${ e } which evaluated to ${ val_1 }" 
+          )
+        }
+      }
+      
+      /**
+       * Evaluate If-Then-Else expressions
+       */
+      case IfThenElse( e1, e2, e3 ) => {
+        val val_1 : Value = eval( e1, env )
+        ( val_1 ) match {
+          case BoolValue( true ) => eval( e2, env )
+          case BoolValue( false ) => eval( e3, env )
+          case _ => throw new IllegalArgumentException(
+            s"If-then-else condition expr: ${ e1 } is non-boolean -- evaluates to ${ val_1 }"
+          )
+        }
+      }
+
+      /**
+       * Evaluate Let expressions
+       */
+      case Let( x, e1, e2 ) => {
+        val val_1 = eval( e1, env )      // eval e1
+        val env_2 = env + ( x -> val_1 ) // create a new extended env
+        eval( e2, env_2 )                // eval e2 under that.
+      }
+  
+      /**
+       * Omit function definitions and function calls
+       */
+      case _:FunDef => {
+        throw new IllegalArgumentException( "Function definitions not yet handled in this interpreter." )
+      }
+      case _:FunCall => { 
+        throw new IllegalArgumentException( "Function calls not yet handled in this interpreter." )
+      }
+    }
+  }
+
+  /**
+   * Pass expressions from the TopLevel program to the eval function with 
+   * an empty environment mapping. 
+   */
+  def evalProgram( p : Program ) = {
+    val m : Map[ String, Value ] = Map[ String, Value ]();
+    ( p ) match {
+      case TopLevel( e ) => {
+        try {
+          eval( e, m );
+        } catch {
+          case e: IllegalArgumentException => {
+            println( s"Error: $e" );
+            ErrorValue
+          }
+        }
+      }
+    }
+  }
+  /* --------------------------------------------------------------- */
+
+  def main( args : Array[ String ] ) : Unit = {
+    /**
+     * Test Lettuce Grammar: Concrete to abstract syntax
+     *
+     * let x = 100 in
+     *  let y = 500 - x * 2 in
+     *    x * y
+     * 
+     * Expected result: 30000
+     */
+    val program_1 = TopLevel(
+                      Let( 
+                        "x", Const( 100 ), 
+                        Let ( 
+                          "y", Minus( Const( 500 ), Mult( Ident( "x" ), Const( 2 ) ) ), 
+                          Mult( Ident( "x" ), Ident( "y" ))
+                        )
+                      )
+                    );
+    println( program_1 );
+
+    /**
+     * Test Lettuce Grammar: Concrete to abstract syntax
+     *
+     * let x = 10 + 15 in
+     *  let y = x >= 25 in
+     *    if (y)
+     *    then x
+     *    else x - 35
+     * 
+     * Expected result: 25
+     */
+    val program_2 = TopLevel(
+                      Let( 
+                        "x", Plus( Const( 10 ), Const( 15 ) ),    // x = 10 + 15
+                        Let( 
+                          "y", Geq( Ident( "x" ), Const( 25 ) ),  // y >= 25
+                          IfThenElse( 
+                            Ident( "y" ),                         // If
+                            Ident( "x" ),                         // then
+                            Minus( Ident( "x" ), Const( 35 ) )    // else
+                          )
+                        )
+                      )
+                    )
+    println( program_2 );
+
+    /**
+     * Test Lettuce Grammar: Concrete to abstract syntax
+     *
+     * let x = 5 in
+     *  let y = 5 in
+     *    let z = x / y in 
+     *      if ( z == 1 )
+     *      then 100
+     *      else -1
+     * Expected result: 100
+     */
+    val program_3 = TopLevel(
+                      Let(
+                        "x", Const( 5 ), 
+                        Let(
+                          "y", Const( 5 ),
+                          Let(
+                            "z", Div( Ident( "x" ), Ident( "y" ) ), 
+                            IfThenElse( 
+                              Eq( Ident( "z" ), Const( 1 ) ), 
+                              Const( 100 ), 
+                              Const( -1 ) )
+                          )
+                        )
+                      )
+                    )
+    println( program_3 );
+
+    /**
+     * Evaluate program_1, program_2, and program_3 using the eval function
+     */
+    val ret_1 : Value = evalProgram( program_1 );
+    val ret_2 : Value = evalProgram( program_2 );
+    val ret_3 : Value = evalProgram( program_3 );
+
+    println( 
+      s"""
+      program_1 evaluated: ${ valConvert( ret_1 ) } 
+      program_2 evaluated: ${ valConvert( ret_2 ) }
+      program_3 evaluated: ${ valConvert( ret_3 ) }
+      """ 
+    );
+  }
+
+}
