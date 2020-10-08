a234bf6fce50b5cfdd30be7a41aab028bd583618
t-cochran <cochran@colorado.edu>
2020-10-08 15:27:32 -0600

    covered tailrec review

--- Week 9/week9_continuationPassingStyle.scala
+++ Week 9/week9_continuationPassingStyle.scala
@@ -1,6 +1,66 @@
 /**
  *  week9_continuationPassingStyle.scala
  */
+import scala.annotation.tailrec
+
+
+object TailRecReview {
+
+  def foo( x : Int ) : Int = {
+    x - 15
+  }
+  /* -------------------------------------------------------------------------------------------------------------- */
+  @tailrec
+  def rec_1( x : Int ) : Int = {
+    if ( x <= 0 )
+      x
+    else
+      rec_1( x - 10 )
+  }
+  @tailrec
+  def rec_2( y : Int = 0, x : Int ) : Int = {
+    if ( x <= 10 )
+      y
+    else
+      rec_2( y + 1, x - 10 )
+  }
+
+  // Not tail recursive
+  def rec_3( x : Int ) : Int = {
+    if ( x <= 10 )
+      x - 10
+    else
+      1 + rec_3( x - 10 )
+  }
+
+  // Not tail recursive
+  def rec_4( x : Int ) : Int = {
+    if ( x <= 10 )
+      x - 5
+    else
+      rec_4( rec_4( x - 10 ) )
+  }
+
+  @tailrec
+  def rec_5( x : Int ) : Int = {
+    if ( x <= 0 )
+      foo( x )
+    else
+      rec_5( foo( x ) )
+  }
+
+  // Not tail recursive
+  def rec_6( x : Int ) : Int = {
+    if ( x <= 0 )
+      foo( x )
+    else
+      foo( rec_6( x - 5 ) )
+  }
+}
+
+
+
+
 object Notes {
   def main( args : Array[ String ] ) : Unit = {
     println( "TODO" )
