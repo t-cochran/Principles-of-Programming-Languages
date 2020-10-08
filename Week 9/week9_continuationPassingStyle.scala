cc615729d2b5144e2129ee8c901b09001eaf9bfc
t-cochran <cochran@colorado.edu>
2020-10-08 16:19:10 -0600

    reviewed tail recursive fibonacci and factorial

--- Week 9/week9_continuationPassingStyle.scala
+++ Week 9/week9_continuationPassingStyle.scala
@@ -5,11 +5,10 @@ import scala.annotation.tailrec
 
 
 object TailRecReview {
-
   def foo( x : Int ) : Int = {
     x - 15
   }
-  /* -------------------------------------------------------------------------------------------------------------- */
+
   @tailrec
   def rec_1( x : Int ) : Int = {
     if ( x <= 0 )
@@ -17,6 +16,7 @@ object TailRecReview {
     else
       rec_1( x - 10 )
   }
+
   @tailrec
   def rec_2( y : Int = 0, x : Int ) : Int = {
     if ( x <= 10 )
@@ -56,10 +56,44 @@ object TailRecReview {
     else
       foo( rec_6( x - 5 ) )
   }
+
+  // Not tail recursive
+  def factorial( n : Int ) : Int = {
+    if ( n <= 0 )
+      1
+    else
+      n * factorial( n - 1 )
+  }
+
+  @tailrec
+  def factorialAcc( acc : Int = 1, n : Int ) : Int = {
+    if ( n <= 0 )
+      acc
+    else
+      factorialAcc( acc * n, n - 1 )
+  }
+
+  // Not tail recurisve
+  def fibonacci( n : Int ) : Int = {
+    if ( n < 2 )
+      1
+    else
+      fibonacci( n - 1 ) + fibonacci( n - 2 )
+  }
+
+  @tailrec
+  def fibonacciAcc( n : Int, acc1 : Int = 1, acc2 : Int = 1 ) : Int = {
+    if (n <= 0)
+      acc1
+    else if (n == 1)
+      acc2
+    else
+      fibonacciAcc( n - 1, acc2, acc1 + acc2 )
+  }
 }
+/* -------------------------------------------------------------------------------------------------------------- */
 
-
-
+/* -------------------------------------------------------------------------------------------------------------- */
 
 object Notes {
   def main( args : Array[ String ] ) : Unit = {
