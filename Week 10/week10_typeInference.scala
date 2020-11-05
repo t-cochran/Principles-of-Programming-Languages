7e1c19d05a2adbb1975aadc509b6f9e563796016
t-cochran <cochran@colorado.edu>
2020-11-05 12:23:05 -0800

    covered grammar for type annotations

--- Week 10/week10_typeInference.scala
+++ Week 10/week10_typeInference.scala
@@ -1,6 +1,34 @@
 /**
- *  Week 10 - Covering course material on type inference
+ *  Week 10 - Covering course material type annotations, type checking, and type inference
  */
+
+/*- Type annotations: Numbers, Booleans, Functions -*/
+sealed trait Type
+case object NumType extends Type
+case object BoolType extends Type
+case class FunType(t1: Type, t2: Type) extends Type
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+sealed trait Expr
+case class Const(f: Double) extends Expr
+case class Ident(s: String) extends Expr
+case class Minus(e1: Expr, e2: Expr) extends Expr
+case class Plus(e1: Expr, e2: Expr) extends Expr
+case class Mult(e1: Expr, e2: Expr) extends Expr
+case class Div(e1: Expr, e2: Expr) extends Expr
+case class Eq(e1: Expr, e2: Expr) extends Expr
+case class Geq(e1: Expr, e2: Expr) extends Expr
+case class IfThenElse(e1: Expr, e2: Expr, e3: Expr) extends Expr
+case class Let(x: String, xType: Type, e1: Expr, e2: Expr) extends Expr
+case class FunDef(id: String, idType: Type, e: Expr) extends Expr
+case class FunCall(calledFun: Expr, argExpr: Expr) extends Expr
+case class LetRec(funName: String, funType: Type, param: String, paramType: Type, funExpr: Expr, bodyExpr: Expr) extends Expr
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+sealed trait Program
+case class TopLevel(e: Expr) extends Program
+/*-------------------------------------------------------------------------------------------------------------------*/
+
 object Notes{
   def sanityTest(): Unit = {
     println("scala code from the week 10 sbt");
