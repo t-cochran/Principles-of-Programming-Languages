2942e20cbbd6ff610b8d1184c3e5dcc19aee9b73
t-cochran <cochran@colorado.edu>
2020-11-11 06:48:09 -0800

    Week 12 initial commit

---
+++ Week 12/build.sbt
@@ -0,0 +1,5 @@
+name := "Week12_Notes"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ Week 12/week12_objectOrientedConcepts.scala
@@ -0,0 +1,10 @@
+/**
+ *  Week 12 - Object oriented concepts material
+ */
+object Notes {
+
+  def main( args: Array[String] ): Unit = {
+    println( "initial commit" )
+  }
+
+}
