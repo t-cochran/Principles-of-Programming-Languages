255e13d8349c0fca31d85ff75584cdc8af310880
t-cochran <cochran@colorado.edu>
2020-11-18 16:58:31 -0800

    reviewing scala objects

--- Week 12/week12_objectOrientedConcepts.scala
+++ Week 12/week12_objectOrientedConcepts.scala
@@ -1,7 +1,13 @@
 /**
  *  Week 12 - Object oriented concepts material
  */
-class Groceries( val chips: String, val water: String, val apples: String) {
+
+/**
+ *  Classes in scala
+ *      -> Can create many instances of objects
+ *      -> Can have private values, getter/setter methods
+ */
+class Groceries( val chips: String, val water: String, val apples: String ) {
 
   println("-"*20 + " filling your grocery cart" + "-" * 20)
   private val rice: String = "koshihikari rice. Needs some soy sauce"
@@ -19,17 +25,43 @@ class Groceries( val chips: String, val water: String, val apples: String) {
 }
 /*-------------------------------------------------------------------------------------------------------------------*/
 
+/**
+ *  Objects in scala
+ *      -> A class that has exactly one instance
+ *      -> Created lazily whenever it is referenced; already created; we cannot create more instances of it
+ *      -> [object x extends z] will create an object 'x' an instance of trait 'z' and can be passed anywhere as a 'z'
+ */
+object Store {
+
+  /*- Enter the store -*/
+  println( "-" * 20 + "You have accessed the wonderful methods of STORE!" + "-" * 20 )
+
+  /*- Give the store a name -*/
+  def name( storeName: String ) : Unit = println( s"Store name is: $storeName ")
+
+  /*- Fill and return a cart of groceries -*/
+  def fillGroceryCart( val chips: String, val water: String, val apples: String ): Groceries = {
+    new Groceries( chips, water, apples )
+  }
+
+}
+
+
+
+
 object Notes {
 
   def main( args: Array[String] ): Unit = {
 
-    /*- Ex 1: Instantiate a class using the 'class' keyword -*/
+    /*- Ex 1: Instantiate a class object -*/
     val shopping_1: Groceries = new Groceries(chips="kettle chips", water="calistoga", apples="honeycrisp")
     println( shopping_1.getChips )
     println( shopping_1.getRice )
     println( shopping_1.getWater )
 
-
+  /*- Ex 2: Access object methods -*/
+  val store_1 = Store.name( storeName="safeway" )
+  val groceryCart: Groceries = Store.fillGroceryCart(chips="corn chips", water="smart water", apples="granny smith")
 
   }
 
